00bd573b1b0a9e7d44a2ddda87fe993ea07d91e6
Kc <kifeakor@yahoo.com>
2023-01-20 16:00:21 +0000

    initial commit

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+let startBtn = document.getElementById("start")
+let timeSpan = document.getElementById("time")
+let time = 0;
+
+startBtn.addEventListener("click", function(event){
+    event.preventDefault()
+
+    
+
+})
+
