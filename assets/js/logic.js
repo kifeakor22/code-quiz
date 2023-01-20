084440cf85119f8a7ad570675c541d3143f96261
Kc <kifeakor@yahoo.com>
2023-01-20 16:14:51 +0000

    added code to start timer when start button is clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,8 +4,28 @@ let time = 0;
 
 startBtn.addEventListener("click", function(event){
     event.preventDefault()
-
-    
-
+    startTimer()
 })
 
+// start time function 
+function startTimer() {   
+  // first set time to start at 0
+   time = 0;  
+  // Use the `setInterval()` method to set time interval
+  var timeInterval = setInterval(function () {  // use the setinterval method that takes a function
+    time++;  // increment time
+    timeSpan.textContent = `${time} seconds`;  // populate html time span with time
+
+
+    // stop countdown once timer hits 0 or condition to stop timer
+
+    if(timeLeft === 27) {
+      clearInterval(timeInterval); // clearinterval takes setinterval as argument
+      // run a function here 
+    }
+
+    //
+    // YOUR CODE HERE
+    //
+  },1000);  // set speed of timer
+}
